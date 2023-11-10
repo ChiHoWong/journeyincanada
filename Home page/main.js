2cd42c7d44405e39f9ee5b03fd239a8b38a6dec4
ChiHoWong <danny.chihow@gmail.com>
2023-11-10 15:55:29 -0500

    update

--- Home page/main.js
+++ Home page/main.js
@@ -4,7 +4,7 @@ function changeLanguage() {
     // Define the URLs for the different language versions of your site
     const languageURLs = {
       'en': '../index.html', // English version
-      'zh': './Home page/homepage-zh.html', // Chinese version
+      'zh': './homePage-zh.html', // Chinese version
       // Add more language URLs as needed
     };
     
