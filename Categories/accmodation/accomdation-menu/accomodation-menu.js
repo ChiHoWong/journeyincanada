96f79ad575a7eaaf2253319a0eec12d58f7c4aad
ChiHoWong <danny.chihow@gmail.com>
2023-11-10 00:46:14 -0500

    Initial commit

---
+++ Categories/accmodation/accomdation-menu/accomodation-menu.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollContainer = document.querySelector(".category-boxs-container");
+    const scrollContent = document.querySelector(".category-box");
+    const scrollLeftButton = document.querySelector(".arrow-left");
+    const scrollRightButton = document.querySelector(".arrow-right");
+
+    scrollLeftButton.addEventListener("click", function () {
+        scrollContainer.scrollLeft -= 100; // Adjust the scroll distance as needed
+        changeButtonPosition();
+    });
+
+    scrollRightButton.addEventListener("click", function () {
+        scrollContainer.scrollLeft += 100; // Adjust the scroll distance as needed
+        changeButtonPosition();       
+    });
+
+  
+});
