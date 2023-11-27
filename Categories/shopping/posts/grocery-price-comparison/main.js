7b9a5254b417bd8566351599ee59691540b800f2
ChiHoWong <danny.chihow@gmail.com>
2023-11-26 23:44:56 -0500

    up

---
+++ Categories/shopping/posts/grocery-price-comparison/main.js
@@ -0,0 +1,13 @@
+function changeLanguage() {
+    const selectedLanguage = document.getElementById("language").value;
+    
+    // Define the URLs for the different language versions of your site
+    const languageURLs = {
+      'en': 'grocery-price-comparison-en.html', // English version
+      'zh': 'grocery-price-comparison-zh.html', // Chinese version
+      // Add more language URLs as needed
+    };
+    
+    // Redirect the user to the selected language version
+    window.location.href = languageURLs[selectedLanguage];
+  }
